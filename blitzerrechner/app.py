31404d26c86f217028bda0b56cb4c99052bb8e13
yalais <yannic.lais@students.fhnw.ch>
2023-05-13 14:13:21 +0200

    new calculation method

--- blitzerrechner/app.py
+++ blitzerrechner/app.py
@@ -39,8 +39,6 @@ def land_weiterleitung():
     land = request.form.get('land')
     if land == 'schweiz':
         return redirect(url_for('index'))
-    elif land == 'ausland':
-        return render_template('ausland.html')
     elif land == 'deutschland':
         return render_template('countries/deutschland.html')
     elif land == 'frankreich':
@@ -56,7 +54,8 @@ def land_weiterleitung():
 # redericting to the speed input page
 @app.route('/eingabe')
 def index():
-    return render_template('eingabe.html')
+    street_types = ['30er Zone', 'Innerorts', 'Ausserorts', 'Autobahn']
+    return render_template('eingabe.html', street_types=street_types)
 
 #calculating the speed difference and redirecting to the street type selection
 #If the difference is 0 or negative, the user is redirected to the page with no fine
@@ -66,9 +65,13 @@ def kategorie():
     erlaubte = int(request.form.get('erlaubt'))
     wiederholung = request.form.get('wiederholung')
     radar = request.form.get('radar')
+    strassentyp = request.form.get('strassentyp')
+
+    #saving the values in the session
     session['gefahrene'] = gefahrene
     session['erlaubte'] = erlaubte
     session['wiederholung'] = wiederholung
+    session['strassentyp'] = strassentyp 
     
 # calculating the speed difference
     if radar == 'laser':
@@ -95,217 +98,204 @@ def kategorie():
 
     session['result'] = result
 
-    if result > 0 and wiederholung == 'nein':
-        return render_template('kategorie.html')
-    elif result <= 0:
-        return render_template('ergebnisse/keine_strafe.html')
-    elif wiederholung == 'ja':
-        return render_template('wiederholungstaeter.html', result=result)
-
-
-
-# Selecting the street type and calculating the fine and the driving ban
-@app.route('/ergebnis', methods=['POST'])
-def ergebnis():
-    answer = request.form.get('kategorie')
-    result = session['result']
-    strafe = 0
-    busse = 0
 #Strafenkatalog für 30er-Zone
-    if answer == '30':
-        if result <= 5:
-            strafe = 'keine weitere Strafe'
-            busse = 40
-        elif result <= 10:
-            strafe = 'keine weitere Strafe'
-            busse = 120
-        elif result <= 15:
-            strafe = 'keine weitere Strafe'
-            busse = 250
-        elif result <= 17:
-            strafe = 'keine weitere Strafe'
-            busse = 400
-        elif result <= 19:
-            strafe = 'keine weitere Strafe'
-            busse = 600
-        elif result <= 20:
-            strafe = 'keine weitere Strafe'
-            busse = '30 Tagessätze'
-        elif result <= 24:
-            strafe = 'keine weitere Strafe'
-            busse = '30 Tagessätze'
-        elif result <= 25:
-            strafe = 'keine weitere Strafe'
-            busse = '30 Tagessätze'
-        elif result <= 29:
-            strafe = 'keine weitere Strafe'
-            busse = '50 Tagessätze'
-        elif result <= 34:
-            strafe = 'keine weitere Strafe'
-            busse = '90 Tagessätze'
-        elif result <= 39:
-            strafe = 'keine weitere Strafe'
-            busse = '120 Tagessätze'
-        else:
-            strafe = 'mindestens 1 Jahr Freiheitsentzug'
-            busse = 'Strafverfahren'
-        return render_template('ergebnisse/ergebnis_30er_zone.html', result=result, strafe=strafe, busse=busse)
-    
-#Strafenkatalog innerorts
-    elif answer == '50':
-        if result <= 5:
-            strafe = 'keine weitere Strafe'
-            busse = 40
-        elif result <= 10:
-            strafe = 'keine weitere Strafe'
-            busse = 120
-        elif result <= 15:
-            strafe = 'keine weitere Strafe'
-            busse = 250
-        elif result <= 17:
-            strafe = 'keine weitere Strafe'
-            busse = 400
-        elif result <= 19:
-            strafe = 'keine weitere Strafe'
-            busse = 400
-        elif result <= 20:
-            strafe = 'keine weitere Strafe'
-            busse = 400
-        elif result <= 24:
-            strafe = '1 Monat Ausweisentzug'
-            busse = 600
-        elif result <= 25:
-            strafe = '3 Monate Ausweisentzug'
-            busse = '20 Tagessätze'
-        elif result <= 29:
-            strafe = '3 Monate Ausweisentzug'
-            busse = '20 Tagessätze'
-        elif result <= 34:
-            strafe = '3 Monate Ausweisentzug'
-            busse = '50 Tagessätze'
-        elif result <= 39:
-            strafe = '3 Monate Ausweisentzug'
-            busse = '70 Tagessätze'
-        elif result <= 44:
-            strafe = 'mindestens 24 Monate Ausweisentzug'
-            busse = 'mindestens 120 Tagessätze'
-        elif result <= 49:
-            strafe = 'mindestens 24 Monate Ausweisentzug'
-            busse = 'mindestens 120 Tagessätze'
-        else:
-            strafe = 'mindestens 1 Jahr Freiheitsentzug'
-            busse = 'Strafverfahren'
-        return render_template('ergebnisse/ergebnis_innerorts.html', result=result, strafe=strafe, busse=busse)
-    
-#Strafenkatalog Auserorts
-    elif answer == '80':
-        if result <= 5:
-            strafe = 'keine weitere Strafe'
-            busse = 40
-        elif result <= 10:
-            strafe = 'keine weitere Strafe'
-            busse = 100
-        elif result <= 15:
-            strafe = 'keine weitere Strafe'
-            busse = 160
-        elif result <= 17:
-            strafe = 'keine weitere Strafe'
-            busse = 240
-        elif result <= 19:
-            strafe = 'keine weitere Strafe'
-            busse = 240
-        elif result <= 20:
-            strafe = 'keine weitere Strafe'
-            busse = 240
-        elif result <= 24:
-            strafe = 'keine weitere Strafe'
-            busse = 400
-        elif result <= 25:
-            strafe = 'keine weitere Strafe'
-            busse = 400
-        elif result <= 29:
-            strafe = '1 Monat Ausweisentzug'
-            busse = 600
-        elif result <= 34:
-            strafe = '3 Monate Ausweisentzug'
-            busse = '10 Tagessätze'
-        elif result <= 39:
-            strafe = '3 Monate Ausweisentzug'
-            busse = '12 Tagessätze'
-        elif result <= 44:
-            strafe = '3 Monate Ausweisentzug'
-            busse = '60 Tagessätze'
-        elif result <= 49:
-            strafe = '3 Monate Ausweisentzug'
-            busse = '90 Tagessätze'
-        elif result <= 54:
-            strafe = '3 Monate Ausweisentzug'
-            busse = 'mindestens 120 Tagessätze'
-        elif result <= 59:
-            strafe = '3 Monate Ausweisentzug'
-            busse = 'mindestens 120 Tagessätze'
-        else:
-            strafe = 'mindestens 1 Jahr Freiheitsentzug'
-            busse = 'Strafverfahren'
-        return render_template('ergebnisse/ergebnis_ausserorts.html', result=result, strafe=strafe, busse=busse)
-    
-# Strafenkatalog Autobahn
-    elif answer == '120':
-        if result <= 5:
-            strafe = 'keine weitere Strafe'
-            busse = 20
-        elif result <= 10:
-            strafe = 'keine weitere Strafe'
-            busse = 60
-        elif result <= 15:
-            strafe = 'keine weitere Strafe'
-            busse = 120
-        elif result <= 17:
-            strafe = 'keine weitere Strafe'
-            busse = 180
-        elif result <= 19:
-            strafe = 'keine weitere Strafe'
-            busse = 180
-        elif result <= 20:
-            strafe = 'keine weitere Strafe'
-            busse = 180
-        elif result <= 24:
-            strafe = 'keine weitere Strafe'
-            busse = 260
-        elif result <= 25:
-            strafe = 'keine weitere Strafe'
-            busse = 260
-        elif result <= 29:
-            strafe = 'keine weitere Strafe'
-            busse = 400
-        elif result <= 34:
-            strafe = '1 Monat Ausweisentzug'
-            busse = 600
-        elif result <= 39:
-            strafe = '3 Monate Ausweisentzug'
-            busse = '20 Tagessätze'
-        elif result <= 44:
-            strafe = '3 Monate Ausweisentzug'
-            busse = '30 Tagessätze'
-        elif result <= 49:
-            strafe = '3 Monate Ausweisentzug'
-            busse = '50 Tagessätze'
-        elif result <= 54:
-            strafe = '3 Monate Ausweisentzug'
-            busse = '60 Tagessätze'
-        elif result <= 59:
-            strafe = '3 Monate Ausweisentzug'
-            busse = '70 Tagessätze'
-        elif result <= 64:
-            strafe = '3 Monate Ausweisentzug'
-            busse = '90 Tagessätze'
-        elif result <= 79:
-            strafe = '3 Monate Ausweisentzug'
-            busse = 'mindestens 120 Tagessätze'
-        else:
-            strafe = 'mindestens 1 Jahr Freiheitsentzug'
-            busse = 'Strafverfahren'
-        return render_template('ergebnisse/ergebnis_autobahn.html', result=result, strafe=strafe, busse=busse)
+    if result <= 0:
+        return render_template('ergebnisse/keine_strafe.html')
+    else:
+        if strassentyp == '30er Zone':
+            if result <= 5:
+                strafe = 'keine weitere Strafe'
+                busse = 40
+            elif result <= 10:
+                strafe = 'keine weitere Strafe'
+                busse = 120
+            elif result <= 15:
+                strafe = 'keine weitere Strafe'
+                busse = 250
+            elif result <= 17:
+                strafe = 'keine weitere Strafe'
+                busse = 400
+            elif result <= 19:
+                strafe = 'keine weitere Strafe'
+                busse = 600
+            elif result <= 20:
+                strafe = 'keine weitere Strafe'
+                busse = '30 Tagessätze'
+            elif result <= 24:
+                strafe = 'keine weitere Strafe'
+                busse = '30 Tagessätze'
+            elif result <= 25:
+                strafe = 'keine weitere Strafe'
+                busse = '30 Tagessätze'
+            elif result <= 29:
+                strafe = 'keine weitere Strafe'
+                busse = '50 Tagessätze'
+            elif result <= 34:
+                strafe = 'keine weitere Strafe'
+                busse = '90 Tagessätze'
+            elif result <= 39:
+                strafe = 'keine weitere Strafe'
+                busse = '120 Tagessätze'
+            else:
+                strafe = 'mindestens 1 Jahr Freiheitsentzug'
+                busse = 'Strafverfahren'
+            return render_template('ergebnisse/ergebnis_30er_zone.html', result=result, strafe=strafe, busse=busse)
+        
+    #Strafenkatalog innerorts
+        elif strassentyp == 'Innerorts':
+            if result <= 5:
+                strafe = 'keine weitere Strafe'
+                busse = 40
+            elif result <= 10:
+                strafe = 'keine weitere Strafe'
+                busse = 120
+            elif result <= 15:
+                strafe = 'keine weitere Strafe'
+                busse = 250
+            elif result <= 17:
+                strafe = 'keine weitere Strafe'
+                busse = 400
+            elif result <= 19:
+                strafe = 'keine weitere Strafe'
+                busse = 400
+            elif result <= 20:
+                strafe = 'keine weitere Strafe'
+                busse = 400
+            elif result <= 24:
+                strafe = '1 Monat Ausweisentzug'
+                busse = 600
+            elif result <= 25:
+                strafe = '3 Monate Ausweisentzug'
+                busse = '20 Tagessätze'
+            elif result <= 29:
+                strafe = '3 Monate Ausweisentzug'
+                busse = '20 Tagessätze'
+            elif result <= 34:
+                strafe = '3 Monate Ausweisentzug'
+                busse = '50 Tagessätze'
+            elif result <= 39:
+                strafe = '3 Monate Ausweisentzug'
+                busse = '70 Tagessätze'
+            elif result <= 44:
+                strafe = 'mindestens 24 Monate Ausweisentzug'
+                busse = 'mindestens 120 Tagessätze'
+            elif result <= 49:
+                strafe = 'mindestens 24 Monate Ausweisentzug'
+                busse = 'mindestens 120 Tagessätze'
+            else:
+                strafe = 'mindestens 1 Jahr Freiheitsentzug'
+                busse = 'Strafverfahren'
+            return render_template('ergebnisse/ergebnis_innerorts.html', result=result, strafe=strafe, busse=busse)
+        
+    #Strafenkatalog Auserorts
+        elif strassentyp == 'Ausserorts':
+            if result <= 5:
+                strafe = 'keine weitere Strafe'
+                busse = 40
+            elif result <= 10:
+                strafe = 'keine weitere Strafe'
+                busse = 100
+            elif result <= 15:
+                strafe = 'keine weitere Strafe'
+                busse = 160
+            elif result <= 17:
+                strafe = 'keine weitere Strafe'
+                busse = 240
+            elif result <= 19:
+                strafe = 'keine weitere Strafe'
+                busse = 240
+            elif result <= 20:
+                strafe = 'keine weitere Strafe'
+                busse = 240
+            elif result <= 24:
+                strafe = 'keine weitere Strafe'
+                busse = 400
+            elif result <= 25:
+                strafe = 'keine weitere Strafe'
+                busse = 400
+            elif result <= 29:
+                strafe = '1 Monat Ausweisentzug'
+                busse = 600
+            elif result <= 34:
+                strafe = '3 Monate Ausweisentzug'
+                busse = '10 Tagessätze'
+            elif result <= 39:
+                strafe = '3 Monate Ausweisentzug'
+                busse = '12 Tagessätze'
+            elif result <= 44:
+                strafe = '3 Monate Ausweisentzug'
+                busse = '60 Tagessätze'
+            elif result <= 49:
+                strafe = '3 Monate Ausweisentzug'
+                busse = '90 Tagessätze'
+            elif result <= 54:
+                strafe = '3 Monate Ausweisentzug'
+                busse = 'mindestens 120 Tagessätze'
+            elif result <= 59:
+                strafe = '3 Monate Ausweisentzug'
+                busse = 'mindestens 120 Tagessätze'
+            else:
+                strafe = 'mindestens 1 Jahr Freiheitsentzug'
+                busse = 'Strafverfahren'
+            return render_template('ergebnisse/ergebnis_ausserorts.html', result=result, strafe=strafe, busse=busse)
+        
+    # Strafenkatalog Autobahn
+        elif strassentyp == 'Autobahn':
+            if result <= 5:
+                strafe = 'keine weitere Strafe'
+                busse = 20
+            elif result <= 10:
+                strafe = 'keine weitere Strafe'
+                busse = 60
+            elif result <= 15:
+                strafe = 'keine weitere Strafe'
+                busse = 120
+            elif result <= 17:
+                strafe = 'keine weitere Strafe'
+                busse = 180
+            elif result <= 19:
+                strafe = 'keine weitere Strafe'
+                busse = 180
+            elif result <= 20:
+                strafe = 'keine weitere Strafe'
+                busse = 180
+            elif result <= 24:
+                strafe = 'keine weitere Strafe'
+                busse = 260
+            elif result <= 25:
+                strafe = 'keine weitere Strafe'
+                busse = 260
+            elif result <= 29:
+                strafe = 'keine weitere Strafe'
+                busse = 400
+            elif result <= 34:
+                strafe = '1 Monat Ausweisentzug'
+                busse = 600
+            elif result <= 39:
+                strafe = '3 Monate Ausweisentzug'
+                busse = '20 Tagessätze'
+            elif result <= 44:
+                strafe = '3 Monate Ausweisentzug'
+                busse = '30 Tagessätze'
+            elif result <= 49:
+                strafe = '3 Monate Ausweisentzug'
+                busse = '50 Tagessätze'
+            elif result <= 54:
+                strafe = '3 Monate Ausweisentzug'
+                busse = '60 Tagessätze'
+            elif result <= 59:
+                strafe = '3 Monate Ausweisentzug'
+                busse = '70 Tagessätze'
+            elif result <= 64:
+                strafe = '3 Monate Ausweisentzug'
+                busse = '90 Tagessätze'
+            elif result <= 79:
+                strafe = '3 Monate Ausweisentzug'
+                busse = 'mindestens 120 Tagessätze'
+            else:
+                strafe = 'mindestens 1 Jahr Freiheitsentzug'
+                busse = 'Strafverfahren'
+            return render_template('ergebnisse/ergebnis_autobahn.html', result=result, strafe=strafe, busse=busse)
 
 
 if __name__ == '__main__':
